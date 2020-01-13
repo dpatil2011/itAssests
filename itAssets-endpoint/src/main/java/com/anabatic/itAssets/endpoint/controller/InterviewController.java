03f5ac1927d092a150fa58f8db4c94706fc54229
joel santhosh raja <joelsanthoshraja96@gmail.com>
2020-01-13 16:43:22 +0530

    hm approve

--- itAssets-endpoint/src/main/java/com/anabatic/itAssets/endpoint/controller/InterviewController.java
+++ itAssets-endpoint/src/main/java/com/anabatic/itAssets/endpoint/controller/InterviewController.java
@@ -1,6 +1,8 @@
 package com.anabatic.itAssets.endpoint.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.anabatic.generic.endpoint.contract.BaseResponse;
+import com.anabatic.itAssets.endpoint.Request.BulkStatusChangeRequest;
 import com.anabatic.itAssets.endpoint.Request.HmApproveRequest;
+import com.anabatic.itAssets.endpoint.Response.BulkStatusChangeResponse;
 import com.anabatic.itAssets.endpoint.converter.InsertCandidateConverter;
 import com.anabatic.itAssets.endpoint.converter.InsertCandidateRecordConverter;
 import com.anabatic.itAssets.endpoint.param.bean.CandidateBean;
@@ -34,55 +38,76 @@ public class InterviewController {
 	private CandidateRecordService candidateRecordService;
 
 	@Autowired
+	private InsertCandidateConverter insertCandidateConverter;
+
+	@Autowired
 	private InsertCandidateRecordConverter insertCandidateRecordConverter;
 
-	@PostMapping("")
+	@PostMapping("HmApprove")
 	public ResponseEntity<BaseResponse> HmApprove(@RequestBody HmApproveRequest request) {
 		Candidate candidate = candidateService.getById(request.getcId());
 		candidate.setStep(request.getStep());
 		candidate.setStatus(request.getStatus());
-		candidateService.update(candidate);
-		CandidateBean bean = new CandidateBean();
-		bean.setName(candidate.getName());
-		bean.setEmail(candidate.getEmail());
-		bean.setPhoneNo(candidate.getPhoneNo());
-		bean.setSkills(candidate.getSkills());
-		bean.setUsers(candidate.getUsers().getId());
-		bean.setUsersr(candidate.getUsersr().getId());
-		bean.setComment(candidate.getComment());
-		bean.setFileName(candidate.getFileName());
-		bean.setUploadDir(candidate.getUploadDir());
-		bean.setFileType(candidate.getFileType());
-		bean.setFilesize(candidate.getFilesize());
-		bean.setInterviewDate(candidate.getInterviewDate());
-		bean.setInterviewStartTime(candidate.getInterviewStartTime());
-		bean.setInterviewEndTime(candidate.getInterviewEndTime());
-		bean.setModeOfInterview(candidate.getModeOfInterview());
-		bean.setSlot(candidate.getSlot());
-		bean.setDateOfJoining(candidate.getDateOfJoining());
-		bean.setCin(candidate.getCin());
-		bean.setStep(candidate.getStep());
-		ObjectMapper objectMapper = new ObjectMapper();
-		String string = null;
-		try {
-			string = objectMapper.writeValueAsString(bean);
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		}
-		CandidateRecord record = new CandidateRecord();
-		record.setcId(candidate);
-		record.setComment(request.getComment());
-		record.setData(string);
-		record.setDate(new Date());
-		Integer step = request.getStep();
-		Integer recordStep = step - 1;
-		record.setStatus(request.getStatus());
-		record.setSteps(recordStep);
-		record.setHmUserId(candidate.getUsers());
-		record.setrUserId(candidate.getUsersr());
-		CandidateRecord record2 = candidateRecordService.insert(record);
+		candidate.setComment(request.getComment());
+		Candidate candidate2 = candidateService.update(candidate);
 		BaseResponse baseResponse = new BaseResponse();
-		baseResponse.setResponse(insertCandidateRecordConverter.toContract(record2));
+		baseResponse.setResponse(insertCandidateConverter.toContract(candidate2));
+		return ResponseEntity.ok(baseResponse);
+
+	}
+
+	@PostMapping("update")
+	public ResponseEntity<BaseResponse> update(@RequestBody List<BulkStatusChangeRequest> requests) {
+		List<BulkStatusChangeResponse> responses = new ArrayList<>();
+		for (BulkStatusChangeRequest request : requests) {
+			Candidate candidate = candidateService.getById(request.getId());
+			candidate.setSelectinStatus(1);
+			CandidateBean bean = new CandidateBean();
+			bean.setName(candidate.getName());
+			bean.setEmail(candidate.getEmail());
+			bean.setPhoneNo(candidate.getPhoneNo());
+			bean.setSkills(candidate.getSkills());
+			bean.setUsers(candidate.getUsers().getId());
+			bean.setUsersr(candidate.getUsersr().getId());
+			bean.setComment(candidate.getComment());
+			bean.setFileName(candidate.getFileName());
+			bean.setUploadDir(candidate.getUploadDir());
+			bean.setFileType(candidate.getFileType());
+			bean.setFilesize(candidate.getFilesize());
+			bean.setInterviewDate(candidate.getInterviewDate());
+			bean.setInterviewStartTime(candidate.getInterviewStartTime());
+			bean.setInterviewEndTime(candidate.getInterviewEndTime());
+			bean.setModeOfInterview(candidate.getModeOfInterview());
+			bean.setSlot(candidate.getSlot());
+			bean.setDateOfJoining(candidate.getDateOfJoining());
+			bean.setCin(candidate.getCin());
+			bean.setStep(candidate.getStep());
+			ObjectMapper objectMapper = new ObjectMapper();
+			String string = null;
+			try {
+				string = objectMapper.writeValueAsString(bean);
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+			}
+			CandidateRecord record = new CandidateRecord();
+			record.setcId(candidate);
+			record.setComment(candidate.getComment());
+			record.setData(string);
+			record.setDate(new Date());
+			Integer step = candidate.getStep();
+			Integer recordStep = step - 1;
+			record.setStatus(candidate.getStatus());
+			record.setSteps(recordStep);
+			record.setHmUserId(candidate.getUsers());
+			record.setrUserId(candidate.getUsersr());
+			CandidateRecord record2 = candidateRecordService.insert(record);
+			BulkStatusChangeResponse response = new BulkStatusChangeResponse();
+			response.setcRId(record2.getId());
+			response.setId(candidate.getId());
+
+		}
+		BaseResponse baseResponse = new BaseResponse();
+		baseResponse.setResponse(responses);
 		return ResponseEntity.ok(baseResponse);
 	}
 }
